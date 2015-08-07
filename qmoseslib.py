52bab6a9405d7ee8420605f3342ab0e6ac246579
JHNfu <furnessjw@gmail.com>
2015-08-07 21:09:48 +0930

    Initial commit

---
+++ qmoseslib.py
@@ -0,0 +1,48 @@
+##########################################################
+# Q-MOSES - A D-WAVE Mesh Partitioner
+##########################################################
+# This is the staff for you to use, Moses.
+# Not developed by a good wind with good timing, or even
+# divine intervention, but rather an understanding of
+# fluid dynamics, applied mathematics, quantum mechanics,
+# & computational complexity theory.
+##########################################################
+# Developed as part of the Honours Project by John Furness
+# With generous in-kind support provided by QxBranch and Shoal Group
+# August 2015
+##########################################################
+# For algorithm explanations and use cases, see attached documentation.
+# Written to PEP8 Python standard.
+# Requires:
+# numpy, networkx, matplotlib, pyplot, dwave_sapi
+##########################################################
+# The MIT License (MIT)
+# Copyright (c) 2015 John Furness
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without restriction,
+# including without limitation the rights to use, copy, modify, merge, publish, distribute,
+# sublicense, and/or sell copies of the Software, and to permit persons to whom the Software
+# is furnished to do so, subject to the following conditions:
+# The above copyright notice and this permission notice shall be included in all copies
+# or substantial portions of the Software.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE
+# AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+##########################################################
+
+# GRAPH PARTITION FUNCTION
+# Imports a variety of graph/mesh/adjacency files, you give the number of partitions,
+# whether to use local or remote solver,
+# Partitions into any number of partitions
+# sub functions include Lucas Hamiltonian for 2^N partitions
+# Your own Hamiltonian for prime numbers etc etc
+
+# FILL-REDUCING ORDERINGS FUNCTION FOR SPARSE MATRICES
+# Using graph partitioning, or graph colouring methods, even clique methods
+
+# MESSAGE SCHEDULING GRAPH COLOURING PROBLEM
+
+# QUANTUM LATTICE GAS for FUN DEMONSTRATION perhaps
